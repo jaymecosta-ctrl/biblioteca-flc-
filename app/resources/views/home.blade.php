d0736dbc22f750c13f12c68bf065978a27ed7121
jaymecosta-ctrl <jayme.costa@edu.mt.gov.br>
2026-09-01 17:18:36 +0000

    dia 10 att1

---
+++ app/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+@section('content')
+    <h1> Seja Bem Vindo a bibliotec FLC</h1>
+    <p>explora nossa coleção de livros</p>
+@endsection
---
+++ app/resources/views/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIBLIOTECA FLC</title>
+</head>
+<body>
+    <header class="retan">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">livros</a></li>
+            <li><a href="/">resenhas</a></li>
+            <li><a href="/">login</a></li>
+        </ul>
+    </header>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
